77a583e6a80ff2e22b83cef89901cac5eefdeae7
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 12:57:29 +0400

    add code for Magazine class

--- src/Entities/Magazine.java
+++ src/Entities/Magazine.java
@@ -1,4 +1,12 @@
 package Entities;
 
 public class Magazine extends ParentItems{
+    private String magazineCode;
+
+    public void setMagazineCode(String magazineCode){
+        this.magazineCode=magazineCode;
+    }
+    public String getMagazineCode(){
+        return magazineCode;
+    }
 }
