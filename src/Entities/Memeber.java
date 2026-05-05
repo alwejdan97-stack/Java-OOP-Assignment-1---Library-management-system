359002b35ef62fcb947217f11798ef0a591a9d34
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 11:26:24 +0400

    add code for Member class

--- src/Entities/Memeber.java
+++ src/Entities/Memeber.java
@@ -1,4 +1,25 @@
 package Entities;
 
-public class Memeber extends ParentAddress{
+import Behaviours.MemberInterface;
+
+public class Memeber extends ParentAddress implements MemberInterface {
+    @Override
+    public void borrowBook(Book book) {
+
+    }
+
+    @Override
+    public void borrowMagazine(Magazine magazine) {
+
+    }
+
+    @Override
+    public void returnBook(Integer bookIdToReturn) {
+
+    }
+
+    @Override
+    public void returnMagazine(Integer magazineIdToReturn) {
+
+    }
 }
