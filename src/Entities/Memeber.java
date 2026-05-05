2ac0c141028478041a3eedf18706c4fb71103b46
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 11:43:41 +0400

    add code for Member class

--- src/Entities/Memeber.java
+++ src/Entities/Memeber.java
@@ -3,6 +3,22 @@ package Entities;
 import Behaviours.MemberInterface;
 
 public class Memeber extends ParentAddress implements MemberInterface {
+    private String dateOfBorrow;
+    private String dateOfReturn;
+
+    public void setDateOfBorrow(String dateOfBorrow){
+        this.dateOfBorrow=dateOfBorrow;
+    }
+    public String getDateOfBorrow(){
+        return dateOfBorrow;
+    }
+    public void setDateOfReturn(String dateOfReturn){
+        this.dateOfReturn=dateOfReturn;
+    }
+    public String getDateOfReturn(){
+        return dateOfReturn;
+    }
+
     @Override
     public void borrowBook(Book book) {
 
