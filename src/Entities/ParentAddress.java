5c9c191690dd54055d6334e6f85532b9700bd2f8
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 12:00:47 +0400

    add code for ParentAddress class

--- src/Entities/ParentAddress.java
+++ src/Entities/ParentAddress.java
@@ -7,32 +7,56 @@ public class ParentAddress {
     private UUID id;
     private String name;
     private String phoneNumber;
+    private List<Magazine> magazineList;
+    private List<Book> bookList;
+    private List<Memeber> memeberList;
 
-    public void setId(UUID id){
-        this.id=id;
+
+    public void setId(UUID id) {
+        this.id = id;
     }
-    public UUID getId(){
+
+    public UUID getId() {
         return id;
     }
-    public void setName(String name){
-        this.name=name;
+
+    public void setName(String name) {
+        this.name = name;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void findById(UUID id){
-        this.id=id;
+
+    public void findById(UUID id) {
+        this.id = id;
     }
-    public void displayByName(String name){
-        this.name=name;
+
+    public void displayByName(String name) {
+        this.name = name;
     }
-    public List<Book> getBookList(){
-        return bookList();
+
+    public void setMagazineList(List<Magazine> magazineList) {
+        this.magazineList = magazineList;
     }
-    public List<Magazine> getMagazineList(){
-        return magazineList();
+
+    public List<Magazine> getMagazineList() {
+        return magazineList;
     }
-    public List<Memeber> getMemeberList(){
-        return memeberList();
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setMemeberList(List<Memeber> memeberList) {
+        this.memeberList = memeberList;
+    }
+
+    public List<Memeber> getMemeberList() {
+        return memeberList;
     }
 }
