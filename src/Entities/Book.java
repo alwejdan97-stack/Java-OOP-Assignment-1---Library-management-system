6b458a659f3215c41c6ad243d62b45cbb7dd8f64
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 12:30:32 +0400

    add code for Book class

--- src/Entities/Book.java
+++ src/Entities/Book.java
@@ -1,4 +1,25 @@
 package Entities;
 
-public class Book extends ParentItems{
+import Behaviours.ItemInterface;
+
+public class Book extends ParentItems implements ItemInterface {
+    @Override
+    public Boolean borrowBookStatus(Book book) {
+        return null;
+    }
+
+    @Override
+    public Boolean borrowBookStatus(Magazine magazine) {
+        return null;
+    }
+
+    @Override
+    public Boolean returnBookStatus(Book book) {
+        return null;
+    }
+
+    @Override
+    public Boolean returnMagazineStatus(Magazine magazine) {
+        return null;
+    }
 }
