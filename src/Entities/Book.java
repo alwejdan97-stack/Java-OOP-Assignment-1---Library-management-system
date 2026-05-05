2b5d516d566a93610d375dad3a41236e5e02c481
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 12:57:03 +0400

    add code for Book class

--- src/Entities/Book.java
+++ src/Entities/Book.java
@@ -4,18 +4,11 @@ import Behaviours.ItemInterface;
 
 public class Book extends ParentItems{
     private String bookCode;
-    private String magazineCode;
 
     public void setBookCode(String bookCode){
         this.bookCode=bookCode;
     }
     public String getBookCode(){
         return bookCode;
-    }
-    public void setMagazineCode(String magazineCode){
-        this.magazineCode=magazineCode;
-    }
-    public String getMagazineCode(){
-        return magazineCode;
     }
 }
