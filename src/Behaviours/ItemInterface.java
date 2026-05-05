1c059dfd4431bcb9e894e73601d46b828fd44293
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 11:16:49 +0400

    create ItemInterface for Book and Magazine

---
+++ src/Behaviours/ItemInterface.java
@@ -0,0 +1,12 @@
+package Behaviours;
+
+import Entities.Book;
+import Entities.Magazine;
+
+public interface ItemInterface {
+    Boolean borrowBookStatus(Book book);
+    Boolean borrowBookStatus(Magazine magazine);
+    Boolean returnBookStatus(Book book);
+    Boolean returnMagazineStatus(Magazine magazine);
+
+}
