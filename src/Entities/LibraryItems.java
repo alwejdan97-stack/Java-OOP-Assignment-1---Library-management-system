ee0336fe8434ead6cf6067f5864a7b25c4b5fe29
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 10:59:25 +0400

    add code for Library class

--- src/Entities/LibraryItems.java
+++ src/Entities/LibraryItems.java
@@ -1,5 +1,28 @@
 package Entities;
 
-public class LibraryItems extends ParentAddress{
+import java.util.List;
 
+public class LibraryItems extends ParentAddress{
+    private List<Magazine> magazineList;
+    private List<Book> bookList;
+    private List<Memeber> memeberList;
+
+    public void setMagazineList(List<Magazine> magazineList){
+        this.magazineList=magazineList;
+    }
+    public List<Magazine> getMagazineList(){
+        return magazineList;
+    }
+    public void setBookList(List<Book> bookList){
+        this.bookList=bookList;
+    }
+    public List<Book> getBookList(){
+        return bookList;
+    }
+    public void setMemeberList(List<Memeber> memeberList){
+        this.memeberList=memeberList;
+    }
+    public List<Memeber> getMemeberList(){
+        return memeberList;
+    }
 }
