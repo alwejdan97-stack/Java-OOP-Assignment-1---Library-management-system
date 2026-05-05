7c6ac6a25569e9987c3c6f9a666e9046fb766ca4
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 15:36:47 +0400

    new modifications code

--- src/Services/LibraryItemService.java
+++ src/Services/LibraryItemService.java
@@ -8,30 +8,52 @@ import java.util.Scanner;
 import java.util.UUID;
 
 public class LibraryItemService {
-    List<LibraryItem> libraryList=new ArrayList<>();
+    List<LibraryItem> itemList =new ArrayList<>();
     LibraryItem libraryItem=new LibraryItem();
     public static Scanner scanner=new Scanner(System.in);
 
-    public List<LibraryItem> addItem(LibraryItem item){
+    /*public List<LibraryItem> addItem(LibraryItem item){
         System.out.println("** Adding new item to the library **");
-        libraryItem.setId(UUID.randomUUID());
 
-        System.out.println("Enter book title");
-        String bookTitle = scanner.nextLine();
-        book.setTitle(bookTitle);
+        System.out.println("Enter item name");
+        String item = scanner.nextLine();
+        libraryItem.setTitle(item);
 
-        System.out.println("Enter book code");
-        String bookCode = scanner.nextLine();
-        book.setBookCode(bookCode);
+        System.out.println("Enter item code");
+        String code = scanner.nextLine();
+        ;
 
         return book;
         System.out.println("");
-        libraryList.add(item);
-        return libraryList;
-    }
+        itemList.add(item);
+        return itemList;
+    }*/
     public LibraryItem findById(String id) {
-        for(LibraryItem l: libraryList){
-            if(){}
+        for(LibraryItem l: itemList){
+            if(l.getId().equals(l)){
+                System.out.println("Item is found");
+                return l;
+            }
+        }
+        return libraryItem;
+    }
+
+    public void searchByTitle(){
+        System.out.println("Enter item to search");
+        String itemToSearch=scanner.nextLine();
+        for(LibraryItem l:itemList){
+            if(l.getTitle().equalsIgnoreCase(itemToSearch)){
+                System.out.println("Item Name: "+l.getTitle()+" | ID: "+l.getId()+" | Status: "l.getStatus());
+            }
+        }
+    }
+
+    public void displayItems(){
+        if(itemList.isEmpty()){
+            System.out.println("NO item in the list");
+        }
+        for(LibraryItem l:itemList){
+            System.out.println("Title: "+l.getTitle()+" | ID: "+l.getId()+" | Status: "+l.getStatus());
         }
     }
 }
