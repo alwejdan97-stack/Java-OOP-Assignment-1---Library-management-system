2073915fe589cbab29515ec0bb360e5f29c9016b
wejdan al-subhi <alwejdan97@gmail.com>
2026-05-05 12:02:58 +0400

    add code for Library class

--- src/Entities/Library.java
+++ src/Entities/Library.java
@@ -5,28 +5,9 @@ import Behaviours.LibraryInterface;
 import java.util.List;
 
 public class Library extends ParentAddress implements LibraryInterface {
-    private List<Magazine> magazineList;
-    private List<Book> bookList;
-    private List<Memeber> memeberList;
-
-    public void setMagazineList(List<Magazine> magazineList){
-        this.magazineList=magazineList;
-    }
-    public List<Magazine> getMagazineList(){
-        return magazineList;
-    }
-    public void setBookList(List<Book> bookList){
-        this.bookList=bookList;
-    }
-    public List<Book> getBookList(){
-        return bookList;
-    }
-    public void setMemeberList(List<Memeber> memeberList){
-        this.memeberList=memeberList;
-    }
-    public List<Memeber> getMemeberList(){
-        return memeberList;
-    }
+    public List<Magazine> magazineList;
+    public List<Book> bookList;
+    public List<Memeber> memeberList;
 
 
     @Override
